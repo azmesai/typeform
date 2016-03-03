b667a79545521e74daf8ec51aec228c5697ed240
aybars badur <aybars.badur@gmail.com>
2016-03-03 17:22:03 +0200

    initial

---
+++ typeform.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import time
+import json
+import urllib
+import re
+
+def lambda_handler(event, context):
+    lastHourDateTime = int(time.time()) - 3600
+    
+    url = 'https://api.typeform.com/v0/form/FORMID?key=APIKEY&completed=true&since=%s' % lastHourDateTime
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+    
+    invite = False
+    sendto = []
+    
+    for response in data['responses']:
+        for key, value in response['answers'].iteritems():
+            if key.encode().startswith('yesno_') and value.encode() == "1":
+                invite = True
+            if key.encode().startswith('email'):
+                email = value.encode()
+        if invite and email:
+            sendto.append( email )
+        invite = False
+        email = ""
+    
+    print sendto
+    
+    for newbie in sendto:
+        data = urllib.urlencode({
+            'email' : newbie,
+            'channels':'DEFAULTCHANNELID',
+            'first_name':'Newbie',
+            'token':'SLACKAPITOKEN',
+            'set_active':'true',
+            '_attempts':'1'
+        })
+        print "Trying for email %s" % newbie
+        f = urllib.urlopen('https://slack.com/api/users.admin.invite?t=%i' % int(time.time()) , data)
+        print f.read()
