779910c3c080b520f56dcc0a1452c9a0a9c03445
Oguz Albayrak <oguzalba@gmail.com>
2016-03-20 18:55:16 +0000

    refactoring and parameterizing the lambda handler for typeform

--- test_typeform.py
+++ test_typeform.py
@@ -5,18 +5,19 @@ from mock import patch, call
 from typeform import lambda_handler
 
 
-def gimmeanswer(yesno, email):
+def gimmeanswer(yesno, email, full_name):
     return {'answers': {
         'yesno_10651767': yesno,
-        'email_10651738': email
+        'email_10651738': email,
+        'textfield_10651691': full_name,
         }
     }
  
 test_input = {
     'responses': [
-        gimmeanswer('1', 'gmail@chucknorris.com'),
-        gimmeanswer('0', 'abc@def.com'),
-        gimmeanswer('1', '')
+        gimmeanswer('1', 'gmail@chucknorris.com', 'Chuck Norris'),
+        gimmeanswer('0', 'abc@def.com', 'abc def'),
+        gimmeanswer('1', '', 'some name')
     ]
 }
 
@@ -28,9 +29,11 @@ class TestCase(unittest.TestCase):
         time.return_value = 3610
         urlopen.return_value.read.return_value = json.dumps(test_input)
         lambda_handler(None, None)
-        self.assertEquals(len(urlopen.mock_calls), 4)
+        self.assertEquals(len(urlopen.mock_calls), 6)
         self.assertEquals(urlopen.mock_calls[0], call('https://api.typeform.com/v0/form/FORMID?key=APIKEY&completed=true&since=10'))
         self.assertEquals(urlopen.mock_calls[1], call().read())
-        self.assertEquals(urlopen.mock_calls[2], call('https://slack.com/api/users.admin.invite?t=3610', 'first_name=Newbie&set_active=true&_attempts=1&channels=DEFAULTCHANNELID&token=SLACKAPITOKEN&email=gmail%40chucknorris.com'))
-        self.assertEquals(urlopen.mock_calls[3], call().read())
+        self.assertEquals(urlopen.mock_calls[2], call().close())
+        self.assertEquals(urlopen.mock_calls[3], call('https://slack.com/api/users.admin.invite?t=3610', 'first_name=Chuck+Norris&set_active=true&_attempts=1&channels=DEFAULTCHANNELID&token=SLACKAPITOKEN&email=gmail%40chucknorris.com'))
+        self.assertEquals(urlopen.mock_calls[4], call().read())
+        self.assertEquals(urlopen.mock_calls[5], call().close())
 
--- typeform.py
+++ typeform.py
@@ -4,39 +4,47 @@ import time
 import json
 import urllib
 import re
+from contextlib import closing
+
+FORMID = "FORMID"
+APIKEY = "APIKEY"
+DEFAULTCHANNELID = "DEFAULTCHANNELID"
+SLACKAPITOKEN = "SLACKAPITOKEN"
+
+FULL_NAME_FIELD = "textfield_10651691"
+AGREE_FIELD = "yesno_10651767"
+EMAIL_FIELD = "email_10651738"
+
+
+def send_invite(email, full_name):
+    data = urllib.urlencode({
+        'email': email,
+        'channels': DEFAULTCHANNELID,
+        'first_name': full_name,
+        'token': SLACKAPITOKEN,
+        'set_active': 'true',
+        '_attempts': '1'
+    })
+    print "Trying for email %s" % email
+    with closing(urllib.urlopen(
+            'https://slack.com/api/users.admin.invite?t=%i' % int(
+                time.time()), data)) as f:
+        print f.read()
 
 
 def lambda_handler(event, context):
     lastHourDateTime = int(time.time()) - 3600
-    
-    url = 'https://api.typeform.com/v0/form/FORMID?key=APIKEY&completed=true&since=%s' % lastHourDateTime
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-    
-    sendto = []
-    for response in data['responses']:
-        invite = False
-        email = ""
-        for key, value in response['answers'].iteritems():
-            if key.encode().startswith('yesno_') and value.encode() == "1":
-                invite = True
-            elif key.encode().startswith('email'):
-                email = value.encode()
-        if invite and email:
-            sendto.append( email )
-    
-    print sendto
-    
-    for newbie in sendto:
-        data = urllib.urlencode({
-            'email' : newbie,
-            'channels':'DEFAULTCHANNELID',
-            'first_name':'Newbie',
-            'token':'SLACKAPITOKEN',
-            'set_active':'true',
-            '_attempts':'1'
-        })
-        print "Trying for email %s" % newbie
-        f = urllib.urlopen('https://slack.com/api/users.admin.invite?t=%i' % int(time.time()) , data)
-        print f.read()
 
+    url = ('https://api.typeform.com/v0/form/'
+           '%s?key=%s&completed=true&since=%s' % (
+               FORMID, APIKEY, lastHourDateTime))
+    with closing(urllib.urlopen(url)) as response:
+        data = json.loads(response.read())
+
+    for response in data['responses']:
+        answers = response['answers']
+        email = answers[EMAIL_FIELD].encode()
+        invite = (answers[AGREE_FIELD] == "1")
+        full_name = answers[FULL_NAME_FIELD]
+        if invite and email:
+            send_invite(email, full_name)
